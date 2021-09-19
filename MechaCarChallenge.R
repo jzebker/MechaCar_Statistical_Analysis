ad14e66710d1c1c32634ae1d5ed0b0eead44c235
Jacob Zebker <jacobzebker@jacobs-air.attlocal.net>
2021-09-19 15:25:48 -0700

    d3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,5 +6,10 @@ summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AW
 SuspensionCoil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 total_summary <- summarize(SuspensionCoil,mean(PSI),median(PSI),var(PSI),sd(PSI))
 
-LotGroups <- group_by(SuspensionCoil,Manufacturing_Lot)
-lot_summary <- summarize(LotGroups,mean(PSI),median(PSI),var(PSI),sd(PSI))
+lots <- group_by(SuspensionCoil,Manufacturing_Lot)
+lot_summary <- summarize(lots,mean(PSI),median(PSI),var(PSI),sd(PSI))
+
+t.test(SuspensionCoil$PSI,mu=total_summary$`mean(PSI)`)
+t.test(subset(SuspensionCoil,Manufacturing_Lot == "Lot1")$PSI,mu=total_summary$`mean(PSI)`)
+t.test(subset(SuspensionCoil,Manufacturing_Lot == "Lot2")$PSI,mu=total_summary$`mean(PSI)`)
+t.test(subset(SuspensionCoil,Manufacturing_Lot == "Lot3")$PSI,mu=total_summary$`mean(PSI)`)
